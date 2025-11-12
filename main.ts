064e3a85bfc5955814381cf8f81f39e7bd6380f6
saulbailen21-cell <saulbailen21@institutramonbarbat.cat>
2025-11-12 13:33:36 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ basic.pause(5000)
 basic.forever(function () {
     obstacle = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (obstacle < 15) {
+        music.play(music.stringPlayable("C5 A B G A F G E ", 250), music.PlaybackMode.UntilDone)
         cuteBot.stopcar()
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
     } else {
